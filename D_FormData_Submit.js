5a3eefc60a53f94bb1cd840ec268a455fe98f2fb
suvankar81 <sarkar.suvankar@gmail.com>
2018-03-27 18:58:05 +0530

    Create D_FormData_Submit.js

---
+++ D_FormData_Submit.js
@@ -0,0 +1,30 @@
+function D_FormData_Submit(d){
+    var form = document.forms.namedItem("fileinfo");
+
+  var oOutput = document.querySelector("div"),
+      oData = new FormData(form);
+
+ // oData.append("CustomField", "This is some extra data");
+
+  var oReq = new XMLHttpRequest();
+  oReq.open("POST", "0.php", true);
+  oReq.onload = function(oEvent) {
+    if (oReq.status == 200) {
+      oOutput.document.getElementById("response").innerHTML = "Uploaded!";
+    } else {
+      oOutput.innerHTML = "Error " + oReq.status + " occurred when trying to upload your file.<br \/>";
+    }
+  };
+  oReq.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          
+           // Typical action to be performed when the document is ready:
+           document.getElementById("response").innerHTML = oReq.response;    
+        }
+        else {
+            document.getElementById("response").innerHTML =  "Error " + oReq.status + " occurred when trying to upload your file.<br \/>";
+            }
+      };
+
+  oReq.send(oData);
+}
